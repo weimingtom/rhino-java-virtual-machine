cf2e1a2a63a2c77452068149ea6026a422ad2f3d
kmcguire <kmcg3413@gmail.com>
2012-05-13 20:14:19 -0400

    (+) worked on java.lang.String more

--- java/lang/String.java
+++ java/lang/String.java
@@ -1,11 +1,65 @@
 package java.lang;
 
 public class String {
-  byte[]        string;
+  byte[]        data;
   public String() {
   }
 
-  public int tester() {
-    return string.length;
+  public String(byte[] data) {
+    this.data = data;
   }
+
+  public int length() {
+    return data.length;
+  }
+
+  public char charAt(int index) {
+    return (char)data[index];
+  }
+
+  public int compareTo(String str) {
+    int         x;
+    int         l;
+    byte[]      a;
+    byte[]      b;
+
+    a = str.data;
+    b = this.data;
+
+    if (a.length < b.length)
+      return 1;
+    if (a.length > b.length)
+      return -1;
+
+    for (x = 0; x < a.length; ++x) {
+      if (a[x] < b[x])
+        return 1;
+      if (a[x] > b[x])
+        return -1;
+    }
+
+    return 0;
+  }
+
+  public String concat(String str) {
+    byte[]      n;
+    int         x;
+    int         al;
+    int         bl;
+
+    al = data.length;
+    bl = str.length();
+
+    n = new byte[al + bl];
+
+    for (x = 0; x < al; ++x) {
+      n[x] = data[x];
+    }
+    for (x = 0; x < bl; ++x) {
+      n[x+al] = str.data[x];
+    }
+
+    return new String(n);
+  }
+
 }
